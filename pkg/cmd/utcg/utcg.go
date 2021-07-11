b7ff02eaf1286fdaea7a6c8da04c8aa2b39dc90a
grimoh <dev@grimoh.net>
2021-07-11 17:02:45 +0900

    Update root command

--- pkg/cmd/utcg/utcg.go
+++ pkg/cmd/utcg/utcg.go
@@ -4,17 +4,16 @@ import (
     "fmt"
     "os"
 
-    homedir "github.com/mitchellh/go-homedir"
     "github.com/spf13/cobra"
-    "github.com/spf13/viper"
 )
 
-var cfgFile string
-
-var rootCmd = &cobra.Command{
-    Use:   "utcg",
-    Short: "utcg is a UNIX time stamp converter using Go.",
-}
+var (
+	rootCmd = &cobra.Command{
+		Use:   "utcg",
+		Short: "utcg is a UNIX time stamp converter using Go.",
+	}
+	nano bool
+)
 
 func Execute() {
     if err := rootCmd.Execute(); err != nil {
@@ -24,29 +23,5 @@ func Execute() {
 }
 
 func init() {
-    cobra.OnInitialize(initConfig)
-
-    rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.utcg.yaml)")
-
-    rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-func initConfig() {
-    if cfgFile != "" {
-        viper.SetConfigFile(cfgFile)
-    } else {
-        home, err := homedir.Dir()
-        if err != nil {
-            fmt.Println(err)
-            os.Exit(1)
-        }
-        viper.AddConfigPath(home)
-        viper.SetConfigName(".utcg")
-    }
-
-    viper.AutomaticEnv()
-
-    if err := viper.ReadInConfig(); err == nil {
-        fmt.Println("Using config file:", viper.ConfigFileUsed())
-    }
+    rootCmd.PersistentFlags().BoolVarP(&nano, "nano", "n", false, "unix time nanoseconds enable flag")
 }
