e64239d5d0c2746a6a0ebb4feaacfccaccc29a42
grimoh <dev@grimoh.net>
2021-07-11 18:10:55 +0900

    Update pkg

--- pkg/cmd/utcg/utcg.go
+++ pkg/cmd/utcg/utcg.go
@@ -1,7 +1,6 @@
 package utcg
 
 import (
-    "fmt"
     "os"
 
     "github.com/spf13/cobra"
@@ -17,7 +16,6 @@ var (
 
 func Execute() {
     if err := rootCmd.Execute(); err != nil {
-        fmt.Println(err)
         os.Exit(1)
     }
 }
